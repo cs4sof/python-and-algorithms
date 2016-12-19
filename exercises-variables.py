6d131c8215ad17c84f424966021e4348b3778025
irimina <irimina@gmail.com>
2016-12-19 16:19:55 -0500

    Add files via upload

---
+++ exercises-variables.py
@@ -0,0 +1,48 @@
+
+# Exercises - Intro to Data Types
+
+# Code the following and add a comment for each line of code
+# Upload the file to your Github account in a repository called
+# Python - data types
+
+'''
+Exercise #1
+Suppose you have $100, which you can invest with a 10% return each year.
+After one year, it's 100×1.1=110100×1.1=110 dollars, and after two years
+it's 100×1.1×1.1=121100×1.1×1.1=121.
+Use Python's print() function
+to calculate how much money you end up with after 7 years.
+
+-------------------------------------------------------------------------
+
+Exercise #2
+Create a variable called savings and equal to 100. 
+Create a variable factor, equal to 1.10.
+Use savings and factor to calculate the amount of money you end up with
+after 7 years. Store the result in a new variable, result.
+Print out the value of result.
+
+-------------------------------------------------------------------------
+Exercise #3
+
+Create a new string, desc, with the value "compound interest".
+Create a new boolean, profitable, with the value True
+
+-------------------------------------------------------------------------
+
+Exercise #4
+
+Find the type of the following variables used in the exercises 1-3 :
+savings, factor, 'compound interest'and profitable.
+Have Python output each data type. 
+
+Exercise #5
+Let's combine date types.
+Add a message that prints the following: 'I started with $( insert your
+savings variable) and now I have $( result variable). Great!'
+
+'''
+
+      
+
+
