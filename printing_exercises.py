eb050e0d4f36a6fa7fa1d67024593e960efa1d19
irimina <irimina@gmail.com>
2016-12-19 16:39:46 -0500

    Add files via upload

---
+++ printing_exercises.py
@@ -0,0 +1,7 @@
+'''
+Go to this link ( copy and paste it in a browser) and solve the challneges using IDLE
+
+https://docs.google.com/document/d/1mS1hqGtd03s5D0eWTuo9oZGNP7-qxOS7NTQEhacEwDQ/edit?usp=sharing
+
+'''
+
