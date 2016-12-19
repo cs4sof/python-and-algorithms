e409559e01fce33af9298fd2125c37358a085a0f
irimina <irimina@gmail.com>
2016-12-19 16:43:28 -0500

    Update printing_exercises.py

--- printing_exercises.py
+++ printing_exercises.py
@@ -1,5 +1,5 @@
 '''
-Go to this link ( copy and paste it in a browser) and solve the challneges using IDLE
+Go to this link ( copy and paste it in a browser) and solve the challneges using IDLE, then upload it to 
 
 https://docs.google.com/document/d/1mS1hqGtd03s5D0eWTuo9oZGNP7-qxOS7NTQEhacEwDQ/edit?usp=sharing
 
