a46e2c3825bd41bac4fb26ff746a5c40fa931a0d
irimina <irimina@gmail.com>
2016-12-19 16:41:09 -0500

    Update printing_exercises.py

--- printing_exercises.py
+++ printing_exercises.py
@@ -1,7 +1,7 @@
 '''
 Go to this link ( copy and paste it in a browser) and solve the challneges using IDLE
 
-<a href="https://docs.google.com/document/d/1mS1hqGtd03s5D0eWTuo9oZGNP7-qxOS7NTQEhacEwDQ/edit?usp=sharing">Python Printing Challenges</a>
+https://docs.google.com/document/d/1mS1hqGtd03s5D0eWTuo9oZGNP7-qxOS7NTQEhacEwDQ/edit?usp=sharing">Python Printing Challenges
 
 '''
 
