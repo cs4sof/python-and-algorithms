d32b1d509a19aa2bf01f42f7a0543970d57fee8c
irimina <irimina@gmail.com>
2016-12-20 14:04:48 -0500

    Update exercises-variables.py

--- exercises-variables.py
+++ exercises-variables.py
@@ -8,8 +8,8 @@
 '''
 Exercise #1
 Suppose you have $100, which you can invest with a 10% return each year.
-After one year, it's 100×1.1=110100×1.1=110 dollars, and after two years
-it's 100×1.1×1.1=121100×1.1×1.1=121.
+After one year, it's 100×1.1=110 dollars, and after two years
+it's 100×1.1×1.1=121.
 Use Python's print() function
 to calculate how much money you end up with after 7 years.
 
